051df5c4e55d32f26580e7c91db9f09b0c019c7a
BhavithaSingamsetty <singamsettybhavitha@gmail.com>
2024-03-22 21:05:35 -0500

    Initial Commit

---
+++ Individual02_Singamsetty_Bhavitha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Individual02_Singamsetty_Bhavitha
+//
+//  Created by Singamsetty, Bhavitha on 3/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
